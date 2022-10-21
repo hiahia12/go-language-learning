ffd0ae3f1309b59a581f44dd576188aa1e0c20be
hiahia12 <284662907@qq.com>
2022-10-21 21:54:44 +0800

    test

---
+++ test_2.go
@@ -0,0 +1,19 @@
+package main
+
+import "time"
+func main(){
+type question struct {
+	title string
+	id int64
+	username string
+	content string
+	ReleaseTime time.Time
+	changeTime time.Time
+	ForwardingVolume int64
+	collection int64
+
+
+
+}
+
+}
